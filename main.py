ae1665ee50ae1573f2fc5653a5abd3794b68553d
sakibtamim <sakib.tamim@gmail.com>
2023-06-20 18:09:07 +0600

    second commit

--- main.py
+++ main.py
@@ -1,54 +1,62 @@
-def sum(a, b, c):
-    return a+b+ c
-def printBoard(xState, zState):
-    zero ='X' if xState[0] else ('O' if zState[0] else 0)
-    one ='X' if xState[1] else ('O' if zState[1] else 1)
-    two ='X' if xState[2] else ('O' if zState[2] else 2)
-    three = 'X' if xState[3] else ('O' if zState[3] else 3)
-    four = 'X' if xState[4] else ('O' if zState[4] else 4)
-    five = 'X' if xState[5] else ('O' if zState[5] else 5)
-    six = 'X' if xState[6] else ('O' if zState[6] else 6)
-    seven = 'X' if xState[7] else ('O' if zState[7] else 7)
-    eight = 'X' if xState[8] else ('O' if zState[8] else 8)
-
-    print (f"{zero} | {one} | {two}")
+def printBoard(gameValues):
+    print(f"{gameValues[0]} | {gameValues[1]} | {gameValues[2]}")
     print(f"--|---|--")
-    print (f"{three} | {four} | {five}")
+    print(f"{gameValues[3]} | {gameValues[4]} | {gameValues[5]}")
     print(f"--|---|--")
-    print (f"{six} | {seven} | {eight}")
+    print(f"{gameValues[6]} | {gameValues[7]} | {gameValues[8]}")
 
-def checkWin(xState, zState):
-    wins = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+def checkWin(gameValues):
+    wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
     for win in wins:
-        if (sum(xState[win[0]],xState[win[1]],xState[win[2]]) ==3):
+        if gameValues[win[0]] == gameValues[win[1]] == gameValues[win[2]] == 'X':
+            printBoard(gameValues)
             print("X won the match")
             return 1
-        if (sum(zState[win[0]],zState[win[1]],zState[win[2]] ) ==3):
+        if gameValues[win[0]] == gameValues[win[1]] == gameValues[win[2]] == 'O':
+            printBoard(gameValues)
             print("0 won the match")
             return 0
+        if all(isinstance(item, str) for item in gameValues):
+            printBoard(gameValues)
+            return -2
     return -1
 
+
 if __name__ == '__main__':
-    xState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    zState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    turn = 1 # 1 for X and 0 for O
+    gameValues = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    turn = 1  # 1 for X and 0 for O
     print("Wellcome to Tic-Tac-Toe")
 
     while True:
-            printBoard(xState, zState)
 
-            if turn == 1:
-                print("X's chance- ")
-                value = int(input("Please enter a value: "))
-                xState[value] = 1
+        if turn == 1:
+            printBoard(gameValues)
+            print("X's chance- ")
+            value = int(input("Please enter a value: "))
+
+            if gameValues[value] != 'O':
+                gameValues[value] = 'X'
             else:
-                print("O's chance- ")
-                value = int(input("Please enter a value: "))
-                zState[value] = 1
+                continue
 
-            cwin = checkWin(xState,zState)
-            if (cwin != -1):
-                print("Match over ")
-            turn = 1 - turn
+        else:
+            printBoard(gameValues)
+            print("O's chance- ")
+            value = int(input("Please enter a value: "))
 
+            if gameValues[value] != 'X':
+                gameValues[value] = 'O'
+            else:
+                continue
+
+        turn = 1 - turn
+        cwin = checkWin(gameValues)
+
+        if cwin == -2:
+            print("Game Draw")
+            break
+        if cwin != -1:
+            print("Match over ")
+            break
